217f947ad320e3bd362ea9c112f9afc631ed7fec
Do Anh Tu <tuda.23bi14438@usth.edu.vn>
2025-09-22 22:17:26 +0700

    prac 4

--- app/src/main/java/vn/edu/usth/usthweather/ForecastFragment.java
+++ app/src/main/java/vn/edu/usth/usthweather/ForecastFragment.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -49,16 +52,14 @@ public class ForecastFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forecast, container, false);
+        LinearLayout layout = new LinearLayout(getActivity());
+        layout.setOrientation(LinearLayout.VERTICAL);
+        View view = inflater.inflate(R.layout.fragment_forecast, container, false);
+        return view;
     }
 }
